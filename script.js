03119cb4bd49e4dd2d42dfc2fba8ee76a5caad11
salvadorvanoli <141060855+salvadorvanoli@users.noreply.github.com>
2023-09-02 17:13:25 -0300

    Primer commit

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+    let API_EDAD_URL = "https://api.agify.io/?name=b";
+    let API_GENERO_URL = "https://api.genderize.io/?name=c"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
